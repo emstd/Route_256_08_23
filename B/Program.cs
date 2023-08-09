026fbbc99cbda712e2a3b79f263d19fdcb2b06ca
Evgeniy Merinov <zhenya_merinov@mail.ru>
2023-08-09 22:46:58 +0700

    B solved

--- B/Program.cs
+++ B/Program.cs
@@ -18,10 +18,32 @@
             }
         }
 
-        static int PriceCalculate(int[] prices)
+        static void PriceCalculate(int[] prices)
         {
             Array.Sort(prices);
 
+            int TotalCounter = prices[0];
+            int ProductCounter = 2;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] == prices[i - 1] && ProductCounter != 3)
+                {
+                    ProductCounter++;
+                    TotalCounter += prices[i];
+                }
+                else if (prices[i] == prices[i - 1] && ProductCounter == 3)
+                {
+                    ProductCounter = 1;
+                    continue;
+                }
+                else
+                {
+                    TotalCounter += prices[i];
+                    ProductCounter = 2;
+                }
+            }
+            Console.WriteLine(TotalCounter);
         }
     }
 }
