7438b2929c911cd3613df391fd04ae9c9d24b87c
Evgeniy Merinov <zhenya_merinov@mail.ru>
2023-08-09 23:19:52 +0700

    C started

---
+++ C/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int DataParts = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < DataParts; i++)
+            {
+                int TotalProgrammers = int.Parse(Console.ReadLine());
+                int[] Skills = new int[TotalProgrammers];
+                string[] SkillsString = Console.ReadLine().Split(' ');
+                for (int j = 0; j < SkillsString.Length; j++)
+                {
+                    Skills[j] = int.Parse(SkillsString[j]);
+                }
+                Grouping(Skills);
+            }
+        }
+
+        static void Grouping(int[] skills)
+        {
+
+        }
+
+    }
+}
