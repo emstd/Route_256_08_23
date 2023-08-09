ac6c1ad1367519e61e3dedca9eb2f44f82a67207
Merinov Evgeniy <evg.merinov@mail.ru>
2023-08-09 12:53:43 +0700

    init commit

---
+++ A/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int DataParts = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < DataParts; i++)
+            {
+                string[] StringNumbers = Console.ReadLine().Split(' ');
+                int FirstNumber = int.Parse(StringNumbers[0]);
+                int SecondNumber = int.Parse(StringNumbers[1]);
+                Console.WriteLine(Add(FirstNumber, SecondNumber));
+            }
+        }
+
+        static int Add(int x, int y) => x + y;
+    }
+}
